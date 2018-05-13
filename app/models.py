a33f6ce5499c3b1147965cf0ed34716dc9ed1d8b
Monroe100 <michelle1hunnid@gmail.com>
2018-05-13 22:18:22 +0300

    added class newsarticle in models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 class newssource:
     '''
-    News class to define news objects
+    newssource class to define news objects
     '''
     def __init__(self,id,name,description,url,category,language,country):
         self.id = id
@@ -11,16 +11,16 @@ class newssource:
         self.language = language
         self.country = country
 
-# class newsarticle:
-#     '''
-#     Article class to define article objects
-#     '''
-#     def __init__(self,id,name,author,title,description,url,urlToImage,):
-#         self.id = id
-#         self.name = name
-#         self.author = author
-#         self.title = title
-#         self.description = description
-#         self.url = url
-#         self.urlToImage = urlToImage
+class newsarticle:
+    '''
+    newsarticle class to define article objects
+    '''
+    def __init__(self,id,name,author,title,description,url,urlToImage,):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
         
