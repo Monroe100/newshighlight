98f49bd4c78c855b97b5161cec8577b8796f2d2b
Monroe100 <michelle1hunnid@gmail.com>
2018-05-13 21:59:53 +0300

    modified init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from Flask_bootstrap import Bootstrap
 from config import config_options
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
-# def create_app(config_name):
+def create_app(config_name):
 
-#     app = Flask(__name__)
+    app = Flask(__name__)
 
 #     #creating the app configurations
 #     app.config.from_object(config_options[config_name])
